812540a0e08a22b49f3f16147b62b679ec35ff1b
Tushar <kmrtushar01@gmail.com>
2024-04-11 12:39:01 +0530

    adding tailwind css

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import TagGif from "./components/TagGif"
 
 function App() {
   return (
-    <div className="App">
-      <h1>RANDOM GIFS</h1>
+    <div className="flex flex-col background w-screen h-screen">
+      <h1 className="bg-white mx-auto w-11/12 font-bold text-4xl mt-[40px] rounded-md px-[4px] py-[8px] text-center shadow-md">RANDOM GIFS</h1>
 
       <div>
         <RandomGif/>
