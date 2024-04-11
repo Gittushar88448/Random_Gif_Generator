d3c27ab1bb7c9614d409e08611a0c1aab68bf8e1
Tushar <kmrtushar01@gmail.com>
2024-04-11 12:26:32 +0530

    adding both the components and heading in app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./index.css"
+import RandomGif from "./components/RandomGif"
+import TagGif from "./components/TagGif"
+
 
 function App() {
   return (
     <div className="App">
-      App
+      <h1>RANDOM GIFS</h1>
+
+      <div>
+        <RandomGif/>
+        <TagGif/>
+      </div>
     </div>
   );
 }
